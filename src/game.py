c0319d1cbda34c047b6e5ebed4f85c9e353de195
goliveira1 <57999337+goliveira1@users.noreply.github.com>
2020-01-08 10:52:20 -0500

    Update game.py

--- src/game.py
+++ src/game.py
@@ -11,15 +11,33 @@ screen = pygame.display.set_mode( (400, 300) )
 while True:
     # do something for each event in the event queue (list of things that happen)
     for event in pygame.event.get():
-        # Check to see if the current event is a QUIT event
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
-    
+        if event.type == pygame.MOUSEMOTION:
+            pos = pygame.mouse.get_pos()
+            if ((50<pos[0] and pos[0]<150) and (50<pos[1] and pos[1]<100)):
+                pygame.draw.rect(screen, (128, 128, 128), (50,50,100,50))
+            if ((150<pos[0] and pos[0]<250) and (100<pos[1] and pos[1]<200)): 
+                pygame.draw.rect(screen, (128, 128, 128), (150,100,100,100))
+            if ((150<pos[0] and pos[0]<250) and (200<pos[1] and pos[1]<250)):
+                pygame.draw.rect(screen, (128, 128, 128), (150,200,100,50))
+            # if ((50<pos[0] and pos[0]<150) and (50<pos[1] and pos[1]<100)):
+            #pygame.draw.rect(screen, (128, 128, 128), (50,50,100,50))
+            # if ((150<pos[0] and pos[0]<250) and (100<pos[1] and pos[1]<200)):
+            #pygame.draw.rect(screen, (128, 128, 128), (50,50,100,50))
+            # if ((200<pos[0] and pos[0]<250) and (200<pos[1] and pos[1]<250)):
+            #pygame.draw.rect(screen, (128, 128, 128), (50,50,100,50))
+            # if ((50<pos[0] and pos[0]<150) and (50<pos[1] and pos[1]<100)):
+            #pygame.draw.rect(screen, (128, 128, 128), (50,50,100,50))
+            # if ((150<pos[0] and pos[0]<250) and (100<pos[1] and pos[1]<200)):
+            #pygame.draw.rect(screen, (128, 128, 128), (50,50,100,50))
+            # if ((200<pos[0] and pos[0]<250) and (200<pos[1] and pos[1]<250)):
+            #pygame.draw.rect(screen, (128, 128, 128), (50,50,100,50))
+        # Check to see if the current event is a QUIT event
     # main code goes here
     pygame.draw.line(screen, (255, 255, 255), (50, 100), (350, 100), 1)
     pygame.draw.line(screen, (255, 255, 255), (50, 200), (350, 200), 1)
     pygame.draw.line(screen, (255, 255, 255), (150, 50), (150, 250), 1)
     pygame.draw.line(screen, (255, 255, 255), (250, 50), (250, 250), 1)
-
     pygame.display.flip()
