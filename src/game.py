120644a4c447032c8a6cb83311eb8e07b232791c
goliveira1 <57999337+goliveira1@users.noreply.github.com>
2020-01-23 08:09:36 -0500

    Update game.py

--- src/game.py
+++ src/game.py
@@ -33,6 +33,7 @@ def give_color(clicked, hovering):
 screen = pygame.display.set_mode( (400, 300) )
 clicked = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 hovered = [False, False, False, False, False, False, False, False, False] # Main loop. Your game would go inside this loop
+total_clicks = 0
 while True:
     # do something for each event in the event queue (list of things that happen)
     for event in pygame.event.get():
@@ -40,25 +41,53 @@ while True:
             # If so, exit the program
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
+            total_clicks += 1
             pos = pygame.mouse.get_pos()
             if ((50<pos[0] and pos[0]<150) and (50<pos[1] and pos[1]<100)):
-                clicked[0] += 1
+                if total_clicks % 2 == 0:
+                    clicked[0] = 2
+                else:
+                    clicked[0] = 1
             if ((150<pos[0] and pos[0]<250) and (100<pos[1] and pos[1]<200)):
-                clicked[4] += 1
+                if total_clicks % 2 == 0:
+                    clicked[4] = 2
+                else:
+                    clicked[4] = 1
             if ((150<pos[0] and pos[0]<250) and (200<pos[1] and pos[1]<250)):
-                clicked[7] += 1
+                if total_clicks % 2 == 0:
+                    clicked[7] = 2
+                else:
+                    clicked[7] = 1
             if ((50<pos[0] and pos[0]<150) and (100<pos[1] and pos[1]<200)):
-                clicked[3] += 1
+                if total_clicks % 2 == 0:
+                    clicked[3] = 2
+                else:
+                    clicked[3] = 1           
             if ((150<pos[0] and pos[0]<250) and (50<pos[1] and pos[1]<100)):
-                clicked[1] += 1
+                if total_clicks % 2 == 0:
+                    clicked[1] = 2
+                else:
+                    clicked[1] = 1
             if ((50<pos[0] and pos[0]<150) and (200<pos[1] and pos[1]<250)):
-                clicked[6] += 1
+                if total_clicks % 2 == 0:
+                    clicked[6] = 2
+                else:
+                    clicked[6] = 1
             if ((250<pos[0] and pos[0]<350) and (50<pos[1] and pos[1]<100)):
-                clicked[2] += 1
+                if total_clicks % 2 == 0:
+                    clicked[2] = 2
+                else:
+                    clicked[2] = 1
             if ((250<pos[0] and pos[0]<350) and (100<pos[1] and pos[1]<200)):
-                clicked[5] += 1
+                if total_clicks % 2 == 0:
+                    clicked[5] = 2
+                else:
+                    clicked[5] = 1
             if ((250<pos[0] and pos[0]<350) and (200<pos[1] and pos[1]<250)):
-                clicked[8] += 1
+                if total_clicks % 2 == 0:
+                    clicked[8] = 2
+                else:
+                    clicked[8] = 1
         if event.type == pygame.MOUSEMOTION:
             pos = pygame.mouse.get_pos()
             if ((50<pos[0] and pos[0]<150) and (50<pos[1] and pos[1]<100)):
